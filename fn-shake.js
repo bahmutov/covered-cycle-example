bd72480ef883465e7bf2a98f2ad641d1c39ce9d2
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2016-04-12 11:16:20 -0400

    chore(shake): removing uncovered functions and generating new source

--- fn-shake.js
+++ fn-shake.js
@@ -4,6 +4,7 @@ const la = require('lazy-ass')
 const is = require('check-more-types')
 const fs = require('fs')
 const esprima = require('esprima')
+const escodegen = require('escodegen')
 
 // const coverFilename = './scripts/coverage.json'
 // const sourceName = 'app.js'
@@ -12,7 +13,12 @@ const sourceName = 'code.js'
 const cover = require(coverFilename)
 
 const sourceFilename = './shake-example/code.js'
-const parsed = esprima.parse(fs.readFileSync(sourceFilename))
+const parseOptions = {
+  loc: true,
+  range: true
+}
+const source = fs.readFileSync(sourceFilename, 'utf8')
+const parsed = esprima.parse(source, parseOptions)
 console.log('parsed', sourceFilename)
 console.log(parsed)
 
@@ -28,7 +34,67 @@ la(is.object(fnMap), 'missing function map')
 console.log('has coverage information about', Object.keys(fnMap).length, 'functions')
 Object.keys(fnMap).forEach((k) => {
   const fn = fnMap[k]
-  console.log('function', fn.name, 'is covered?', f[k])
+  console.log('function', fn.name, fn, 'is covered?', f[k])
 })
 
+function findCoveredFunction(line, column) {
+  la(is.number(line) && is.number(column),
+    'missing function start line or column', line, column)
+  var found
+  Object.keys(fnMap).some((k) => {
+    const fn = fnMap[k]
+    if (fn.loc.start.line === line &&
+      fn.loc.start.column === column) {
+      found = {
+        fn: fn,
+        covered: f[k]
+      }
+      return true
+    }
+  })
+  return found
+}
 
+function setCharAt(str,index,chr) {
+  if(index > str.length-1) return str;
+  return str.substr(0,index) + chr + str.substr(index+1);
+}
+
+function blank (text, from, to) {
+  la(is.unemptyString(text), 'missing text', text)
+  la(is.number(from), 'missing from index', from)
+  la(is.number(to), 'missing to index', to)
+  la(to >= from, 'invalid from and to', from, to)
+
+  var k
+  for (k = from; k < to; k += 1) {
+    text = setCharAt(text, k, ' ')
+  }
+  return text
+}
+
+var sourceBlanked = source
+function walk(node, parent, index) {
+  console.log(node.type)
+  if (node.type === 'FunctionDeclaration') {
+    console.log(node.id.name, 'at', node.loc, node.range)
+    const info = findCoveredFunction(node.loc.start.line, node.loc.start.column)
+    if (info && !info.covered) {
+      console.log('removing function', node.id.name, 'from source')
+      // sourceBlanked = blank(sourceBlanked, Number(node.range[0]), Number(node.range[1]))
+      // console.log(parent)
+      parent.body.splice(index, 1)
+    }
+  }
+  if (Array.isArray(node.body)) {
+    node.body.forEach((child, k) => walk(child, node, k))
+  }
+}
+walk(parsed)
+
+console.log('removed uncovered functions')
+console.log(sourceBlanked)
+
+const output = escodegen.generate(parsed)
+console.log('generated')
+console.log(output)
--- shake-example/code.js
+++ shake-example/code.js
@@ -1,9 +1,7 @@
 function add (a, b) {
   return a + b
 }
-
 function sub (a, b) {
   return a - b
 }
-
-console.log('2 + 3', add(2, 3))
+console.log('2 + 3 =', add(2, 3))
